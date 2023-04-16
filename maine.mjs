06de41684185c2be54c50fb5eb961953b8b3b960
Kyle Jacobson <kylejacobson1987@gmail.com>
2023-04-16 11:40:09 -0700

    add Maine scraper

---
+++ maine.mjs
@@ -0,0 +1,49 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // TODO: older archives in XSL format
+  await page.goto('https://apps.web.maine.gov/online/aeviewer/ME/40/list.shtml', { waitUnti: 'networkidle0' });
+  await page.setViewport({width: 1280, height: 1024});
+  const pageSizeSelect = await page.waitForSelector('[name="DataTables_Table_0_length"]');
+  await pageSizeSelect.select("100");
+  const lastPage = await page.waitForSelector('#DataTables_Table_0_paginate > span > a.paginate_button:last-child');
+  await page.evaluate((el) => el.click(), lastPage);
+  await page.waitForSelector('#DataTables_Table_0_next.disabled');
+
+  let done = false;
+  while (!done) {
+      const selectedPageLink = await page.waitForSelector('.paginate_button.current');
+      const selectedPageNumber = await page.evaluate((el) => el.textContent, selectedPageLink);
+      console.log("Gathering data from page " + selectedPageNumber + "...");
+      const table = await page.waitForSelector('#DataTables_Table_0');
+      await page.waitForFunction(() => (
+          document.querySelectorAll('#DataTables_Table_0 > tbody > tr').length > 0
+      ));
+      const rows = await table.$$('tbody > tr');
+      for (const row of rows) {
+        try {
+            const [reported, bizName] = await row.$$("td");
+            let businessName = await page.evaluate((el) => el.textContent, bizName);
+            businessName = businessName.trim();
+            let reportedDate = await page.evaluate((el) => el.textContent, reported);
+            reportedDate = reportedDate.trim();
+            console.log({
+              businessName,
+              reportedDate,
+            });
+        }
+        catch(e) { console.error(e); }
+      }
+      if (selectedPageNumber !== "1") {
+          const prevPageLink = await page.evaluateHandle(el => el.previousElementSibling, selectedPageLink);
+          await prevPageLink.click()
+      } else {
+          done = true;
+      }
+  }
+
+  await browser.close();
+})();
