35e5a2578253674b030552f571f80832e591d899
Kyle Jacobson <kylejacobson1987@gmail.com>
2023-04-16 00:37:41 -0700

    add california scraper

---
+++ california.mjs
@@ -0,0 +1,35 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://oag.ca.gov/privacy/databreach/list', { waitUnti: 'networkidle0' });
+  await page.setViewport({width: 1280, height: 1024});
+
+  try {
+      const table = await page.waitForSelector('#block-system-main .views-table');
+      await page.waitForFunction(() => (
+          document.querySelectorAll('#block-system-main .views-table > tbody > tr').length > 0
+      ));
+      const rows = await table.$$('tbody > tr');
+      for (const row of rows) {
+            const [bizName, dates, reported] = await row.$$("td");
+            let businessName = await page.evaluate((el) => el.textContent, bizName);
+            businessName = businessName.trim();
+            let breachDates = await page.evaluate((el) => el.textContent, dates);
+            breachDates = breachDates.trim().split(', ');
+            let reportedDate = await page.evaluate((el) => el.textContent, reported);
+            reportedDate = reportedDate.trim();
+            console.log({
+              businessName,
+              breachDates,
+              reportedDate,
+            });
+        }
+      }
+  catch(e) { console.error(e); }
+
+  await browser.close();
+})();
+
