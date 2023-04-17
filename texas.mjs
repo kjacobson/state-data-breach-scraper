f8cad2e48a3779a20428760c461391bb3caa039e
Kyle Jacobson <kylejacobson1987@gmail.com>
2023-04-17 16:30:19 -0700

    add TX scraper

---
+++ texas.mjs
@@ -0,0 +1,59 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://oag.my.site.com/datasecuritybreachreport/apex/DataSecurityReportsPage', { waitUntil: 'networkidle0' });
+  await page.setViewport({width: 1280, height: 1024});
+  const lastPage = await page.waitForSelector('.paginate_button.last');
+  const nav = page.waitForSelector('.paginate_button.last.disabled');
+  await page.evaluate((el) => el.click(), lastPage);
+  await nav;
+
+  let done = false;
+  while (!done) {
+      const selectedPageLI = await page.waitForSelector('.paginate_button.current');
+      const selectedPageNumber = await page.evaluate((el) => el.textContent, selectedPageLI);
+      console.log("Gathering data from page " + selectedPageNumber + "...");
+      const table = await page.waitForSelector('#mycdrs');
+      await page.waitForFunction(() => (
+          document.querySelectorAll('#mycdrs > tbody > tr').length > 0
+      ));
+      const rows = await table.$$('tbody > tr');
+      for (const row of rows) {
+        try {
+            const [bizName, address, city, state, zip, dataTypes, affected, _, __, published] = await row.$$("td");
+            const businessName = await page.evaluate((el) => el.textContent.trim(), bizName);
+            const businessAddress = await page.evaluate((el) => el.textContent.trim(), address);
+            const businessCity = await page.evaluate((el) => el.textContent.trim(), city);
+            const businessState = await page.evaluate((el) => el.textContent.trim(), state);
+            const businessZip = await page.evaluate((el) => el.textContent.trim(), zip);
+            const publishedDate = await page.evaluate((el) => el.textContent.trim(), published);
+            let numberAffected = await page.evaluate((el) => el.textContent.trim(), affected);
+            numberAffected = parseInt(numberAffected, 10);
+            let dataAccessed = await page.evaluate((el) => el.textContent.trim(), dataTypes);
+            dataAccessed = dataAccessed.split(';');
+            console.log({
+              businessName,
+              businessAddress,
+              businessCity,
+              businessState,
+              businessZip,
+              publishedDate,
+              numberAffected,
+              dataAccessed,
+            });
+        }
+        catch(e) { console.error(e); }
+      }
+      if (selectedPageNumber !== "1") {
+          const prevPageLink = await page.waitForSelector('.paginate_button.previous');
+          await prevPageLink.click()
+      } else {
+          done = true;
+      }
+  }
+
+  await browser.close();
+})();
